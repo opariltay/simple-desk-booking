c60f424376290c4cc4daeac603a2a463f8a0897d
Emre Onur PARILTAY <eopariltay@gmail.com>
2022-11-13 23:40:59 +0300

    is_admin field added to User model.

--- app/Http/Middleware/CheckIfAdmin.php
+++ app/Http/Middleware/CheckIfAdmin.php
@@ -27,8 +27,8 @@ class CheckIfAdmin
      */
     private function checkIfUserIsAdmin($user)
     {
-        // return ($user->is_admin == 1);
-        return true;
+        return ($user->is_admin == 1);
+        //return true;
     }
 
     /**
