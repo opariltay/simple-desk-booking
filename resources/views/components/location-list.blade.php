4e85f838a7cf7664939d1dac8586f66689077e30
onur.pariltay <onur.pariltay@bitlo.com>
2022-11-17 15:56:08 +0300

    Calendar positioning updated. CSS will be committed separately.

--- resources/views/components/location-list.blade.php
+++ resources/views/components/location-list.blade.php
@@ -16,7 +16,7 @@ $locations = App\Models\Location::orderBy('name', 'ASC')->get();
                         </button>
                     </h2>
                     <div id="accordion-color-body-{{ $location->id }}" class="" aria-labelledby="accordion-color-heading-{{ $location->id }}">
-                        <div class="p-5 font-light border border-b-0 border-gray-200 dark:border-gray-700 dark:bg-gray-900">
+                        <div class="px-5 pb-5 font-light border border-b-0 border-gray-200 dark:border-gray-700 dark:bg-gray-900">
                             <x-reservation-calendar></x-reservation-calendar>
                         </div>
                     </div>
