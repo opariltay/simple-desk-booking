734c458f38b474731a9e96fe83f4fb9233b7df15
onur.pariltay <onur.pariltay@bitlo.com>
2022-11-24 17:20:29 +0300

    Admin panel menu icons updated.

--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -1,6 +1,6 @@
 {{-- This file is used to store sidebar items, inside the Backpack admin panel --}}
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="la la-home nav-icon"></i> {{ trans('backpack::base.dashboard') }}</a></li>
 
-<li class="nav-item"><a class="nav-link" href="{{ backpack_url('user') }}"><i class="nav-icon la la-question"></i> Users</a></li>
-<li class="nav-item"><a class="nav-link" href="{{ backpack_url('location') }}"><i class="nav-icon la la-question"></i> Locations</a></li>
-<li class="nav-item"><a class="nav-link" href="{{ backpack_url('reservation') }}"><i class="nav-icon la la-question"></i> Reservations</a></li>
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('user') }}"><i class="nav-icon la la-user"></i> Users</a></li>
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('location') }}"><i class="nav-icon la la-building"></i> Locations</a></li>
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('reservation') }}"><i class="nav-icon la la-calendar-check"></i> Reservations</a></li>
